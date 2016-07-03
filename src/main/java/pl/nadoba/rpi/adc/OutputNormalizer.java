ea0ed71d7070786c02cdfb839514b0ed045c5a0f
Adam Nadoba <anadoba@windowslive.com>
2016-07-03 18:57:50 +0200

    Optimize the printed results

--- src/main/java/pl/nadoba/rpi/adc/OutputNormalizer.java
+++ src/main/java/pl/nadoba/rpi/adc/OutputNormalizer.java
@@ -1,9 +1,12 @@
 package pl.nadoba.rpi.adc;
 
+import java.lang.Math;
+
 public class OutputNormalizer {
 
     public static String toPercent(double d) {
-        double percent = (d / 1023) * 100;
-        return String.format("%.1f%", d);
+        double inputPercent = (d / 1023d) * 100d;
+	double result = Math.abs(100 - inputPercent);
+        return String.format("%.0f", result) + "%";
     }
 }
